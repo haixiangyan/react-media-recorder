d83f7f8a114d19961081a9395e853915a95e5f0b
haixiangyan <haixiang6123@gmail.com>
2022-02-28 22:42:53 +0800

    feat: 添加暂停恢复功能

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,38 @@ import React, {useRef, useState} from 'react';
 const App = () => {
   const [audioUrl, setAudioUrl] = useState<string>('');
 
-  const stream = useRef<MediaStream>();
-  const recorder = useRef<MediaRecorder>();
+  const mediaStream = useRef<MediaStream>();
+  const mediaRecorder = useRef<MediaRecorder>();
   const mediaBlobs = useRef<Blob[]>([]);
 
   const startRecord = async () => {
-    stream.current = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
-    recorder.current = new MediaRecorder(stream.current);
+    mediaStream.current = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+    mediaRecorder.current = new MediaRecorder(mediaStream.current);
 
-    recorder.current.ondataavailable = (blobEvent) => {
+    mediaRecorder.current.ondataavailable = (blobEvent) => {
       mediaBlobs.current.push(blobEvent.data);
     }
-    recorder.current.onstop = () => {
+    mediaRecorder.current.onstop = () => {
       const blob = new Blob(mediaBlobs.current, { type: 'audio/wav' })
       const mediaUrl = URL.createObjectURL(blob);
       setAudioUrl(mediaUrl);
     }
 
-    recorder.current?.start();
+    mediaRecorder.current?.start();
+  }
+
+  const pauseRecord = async () => {
+    mediaRecorder.current?.pause();
+  }
+
+  const resumeRecord = async () => {
+    mediaRecorder.current?.resume()
   }
 
   const stopRecord = async () => {
-    recorder.current?.stop()
-    stream.current?.getTracks().forEach((track) => track.stop());
+    mediaRecorder.current?.stop()
+    mediaStream.current?.getTracks().forEach((track) => track.stop());
+    mediaBlobs.current = [];
   }
 
   return (
@@ -35,8 +44,8 @@ const App = () => {
       <audio src={audioUrl} controls />
 
       <button onClick={startRecord}>开始</button>
-      <button>暂停</button>
-      <button>回复</button>
+      <button onClick={pauseRecord}>暂停</button>
+      <button onClick={resumeRecord}>恢复</button>
       <button onClick={stopRecord}>停止</button>
     </div>
   );
